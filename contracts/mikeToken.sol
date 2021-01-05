85f749f1ec96b57aeb2ace223edee8df5fbadd00
Mike <mbo221@lehigh.edu>
2021-01-05 15:17:14 -0500

    Delegated Transfer

--- contracts/mikeToken.sol
+++ contracts/mikeToken.sol
@@ -1,7 +1,6 @@
 pragma solidity ^0.5.16;
 
 contract mikeToken {
-    //declaring state variable public will implicitly create a getter for totalSupply
     uint256 public totalSupply;
 
     string public name = "Mike Token";
@@ -10,10 +9,19 @@ contract mikeToken {
 
     string public standard = "Mike Token v1.0";
 
-    //key:value mapping
+    /// @notice balanceOf maps addresses to balances
     mapping(address => uint256) public balanceOf;
 
+    /// @notice Allowance maps owner adresses to the allowance a uint256 amount for a given spender address
+    mapping(address => mapping(address => uint256)) public allowance;
+
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
+
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
 
     constructor(uint256 _initialSupply) public {
         //sends 1000000 mikeToken to the EOA that uploaded contract to blockchain. Initial amount was
@@ -35,6 +43,44 @@ contract mikeToken {
         balanceOf[_to] += _value;
 
         emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    /// @notice approve() allows _spender to withdraw from you account up to the _value amount.
+    /// The allowance is then stored globalling in the allowance mapping
+    /// @param _spender address that is approved to spend valued amount
+    /// @param _value amount of tokens spender is allowed to spend
+
+    function approve(address _spender, uint256 _value)
+        public
+        returns (bool success)
+    {
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    /// @notice tranferFrom handles a delegated token transfer where an approved spending account can withdraw
+    /// @notice and transfer funds from a given account _from to a and a given account _to
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    ) public returns (bool success) {
+        //require value not to exceed balance of owner's account
+        require(_value <= balanceOf[_from]);
+        //return value not to exceed allowance
+        require(_value <= allowance[_from][msg.sender]);
+
+        //change balance
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+        
+        //update allowance
+        allowance[_from][msg.sender] -= _value;
+
+        emit Transfer(_from, _to, _value);
+
         return true;
     }
 }
--- test/mikeToken.js
+++ test/mikeToken.js
@@ -2,63 +2,254 @@ var mikeToken = artifacts.require("./mikeToken.sol");
 
 //initialize test suite
 //callback function will pass back all accounts available to ganache
-contract('mikeToken', function(accounts){
-    var tokenInstance;
-    it('Intializes the contract with correct values',() => {
-        return mikeToken.deployed().then(instance =>{
-            tokenInstance = instance
-            return tokenInstance.name()
-        }).then (name =>{
-            assert.equal(name, 'Mike Token','check token name')
-            return tokenInstance.symbol()
-        }).then(symbol => {
-            assert.equal(symbol,'MIKE','check token symbol')
-            return tokenInstance.standard();
-        }).then(standard => {
-            assert.equal(standard,'Mike Token v1.0','check token standard')
-        }
+contract("mikeToken", function (accounts) {
+  var tokenInstance;
+  it("Intializes the contract with correct values", () => {
+    return mikeToken
+      .deployed()
+      .then((instance) => {
+        tokenInstance = instance;
+        return tokenInstance.name();
+      })
+      .then((name) => {
+        assert.equal(name, "Mike Token", "check token name");
+        return tokenInstance.symbol();
+      })
+      .then((symbol) => {
+        assert.equal(symbol, "MIKE", "check token symbol");
+        return tokenInstance.standard();
+      })
+      .then((standard) => {
+        assert.equal(standard, "Mike Token v1.0", "check token standard");
+      });
+  });
+  it("Allocates the initial supply upon deployment", function () {
+    return mikeToken
+      .deployed()
+      .then((instance) => {
+        tokenInstance = instance;
+        return tokenInstance.totalSupply();
+      })
+      .then((totalSupply) => {
+        assert.equal(
+          totalSupply.toNumber(),
+          1000000,
+          "sets the total supply of 1,000,000"
+        );
+        return tokenInstance.balanceOf(accounts[0]);
+      })
+      .then((adminBalance) => {
+        assert.equal(
+          adminBalance.toNumber(),
+          1000000,
+          "it allocates the initial supupply to admin account "
+        );
+      });
+  });
 
-        )
+  it("transfers token ownership", () => {
+    return mikeToken
+      .deployed()
+      .then((instance) => {
+        tokenInstance = instance;
+        //should raise a error because the transfer amount far exceeds any possible balance of an given EOA
+        //Note - .call allows us to call our function and inspect but does not create a transaction, so there will be no receipt
+        return tokenInstance.transfer.call(accounts[1], 23939239293393);
+      })
+      .then(assert.fail)
+      .catch((error) => {
+        assert(
+          error.message.indexOf("revert") >= 0,
+          "error must contain substring revert, if revert will be at an index greater than 0"
+        );
+        //Note - .call allows us to call our function and inspect but does not create a transaction,
+        //so there will be no write to the blockchain and hence no transaction receipt
+        return tokenInstance.transfer.call(accounts[1], 250000, {
+          from: accounts[0],
+        });
+      })
+      .then((success) => {
+        assert.equal(success, true, "it returns true");
+        //Calls the transfer function naturally and return a receipt
+        return tokenInstance.transfer(accounts[1], 250000, {
+          from: accounts[0],
+        });
+      })
+      .then((receipt) => {
+        assert.equal(
+          receipt.logs.length,
+          1,
+          "since there is only one event, the transaction reciept should be length 1"
+        );
+        assert.equal(
+          receipt.logs[0].event,
+          "Transfer",
+          'should be the "Transfer" event'
+        );
+        assert.equal(
+          receipt.logs[0].args._from,
+          accounts[0],
+          "logs the account the tokens are transferred from"
+        );
+        assert.equal(
+          receipt.logs[0].args._to,
+          accounts[1],
+          "logs the account the tokens are transferred to"
+        );
+        assert.equal(
+          receipt.logs[0].args._value,
+          250000,
+          "logs the transfer amount"
+        );
+        return tokenInstance.balanceOf(accounts[1]);
+      })
+      .then((balance) => {
+        assert.equal(
+          balance.toNumber(),
+          250000,
+          "adds the amount to the receiving account"
+        );
+        return tokenInstance.balanceOf(accounts[0]);
+      })
+      .then((balance) => {
+        assert.equal(
+          balance.toNumber(),
+          750000,
+          "deducts the amount from the sending account"
+        );
+      });
+  });
+
+  it("approves tokens for delegated transfer", function () {
+    return mikeToken
+      .deployed()
+      .then((instance) => {
+        tokenInstance = instance;
+        return tokenInstance.approve.call(accounts[1], 100);
+      })
+      .then((success) => {
+        assert.equal(success, true, "it returns true");
+        return tokenInstance.approve(accounts[1], 100, { from: accounts[0] });
+      })
+      .then((receipt) => {
+        assert.equal(
+          receipt.logs.length,
+          1,
+          "since there is only one event, the transaction reciept should be length 1"
+        );
+        assert.equal(
+          receipt.logs[0].event,
+          "Approval",
+          'should be the "Approval" event'
+        );
+        assert.equal(
+          receipt.logs[0].args._owner,
+          accounts[0],
+          "logs the account the tokens are authorized by"
+        );
+        assert.equal(
+          receipt.logs[0].args._spender,
+          accounts[1],
+          "logs the account the tokens are authorized to"
+        );
+        assert.equal(
+          receipt.logs[0].args._value,
+          100,
+          "logs the transfer amount"
+        );
+
+        return tokenInstance.allowance(accounts[0], accounts[1]);
+      })
+      .then((allowance) => {
+        assert.equal(
+          allowance.toNumber(),
+          100,
+          "stores the allowance for delegated transfer"
+        );
+      });
+  });
+
+  it("handles delegated token transfer", () => {
+    return mikeToken
+      .deployed()
+      .then((instance) => {
+        tokenInstance = instance;
+        fromAccount = accounts[2];
+        toAccount = accounts[3];
+        spendingAccount = accounts[4];
+        // Transfer so tokens to fromAccount
+        return tokenInstance.transfer(fromAccount, 100, { from: accounts[0] });
+      })
+      .then((receipt) => {
+        // Approve spendingAccount to spend 10 tokens from fromAccount
+        return tokenInstance.approve(spendingAccount, 10, {
+          from: fromAccount,
+        });
+      })
+      .then((receipt) => {
+        // Try transferring something larger that the sender's balance
+        return tokenInstance.transferFrom(fromAccount, toAccount, 99999, {
+          from: spendingAccount,
+        });
+      })
+      .then(assert.fail)
+      .catch((error) => {
+        assert(
+          error.message.indexOf("revert") >= 0,
+          "error must contain substring revert, if revert will be at an index greater than 0"
+        );
+        // Try transferring something larger than the approved amount
+        return tokenInstance.transferFrom(fromAccount, toAccount, 20, {
+          from: spendingAccount,
+        });
+      })
+      .then(assert.fail)
+      .catch((error) => {
+        assert(
+          error.message.indexOf("revert") >= 0,
+            "Cannot transfer value larger than approved amount"
+        );
+        return tokenInstance.transferFrom.call(fromAccount, toAccount, 10, {
+            from: spendingAccount,
+          });
+      }).then(success => {
+          assert.equal(success,true)
+          return tokenInstance.transferFrom(fromAccount, toAccount, 10 , {from:spendingAccount})
+      }).then(receipt => {
+        assert.equal(
+            receipt.logs.length,
+            1,
+            "since there is only one event, the transaction reciept should be length 1"
+          );
+          assert.equal(
+            receipt.logs[0].event,
+            "Transfer",
+            'should be the "Transfer" event'
+          );
+          assert.equal(
+            receipt.logs[0].args._from,
+            fromAccount,
+            "logs the account the tokens are authorized by"
+          );
+          assert.equal(
+            receipt.logs[0].args._to,
+            toAccount,
+            "logs the account the tokens are authorized to"
+          );
+          assert.equal(
+            receipt.logs[0].args._value,
+            10,
+            "logs the transfer amount"
+          );
+          return tokenInstance.balanceOf(fromAccount)
+      }).then(balance => {
+          assert.equal(balance.toNumber(), 90, 'deducts the amount from the sending account')
+          return tokenInstance.balanceOf(toAccount);
+      }).then(balance => {
+        assert.equal(balance.toNumber(), 10, 'adds the amount from the sending account')
+        return tokenInstance.allowance(fromAccount, spendingAccount)
+    }).then(allowance => {
+        assert.equal(allowance.toNumber(), 0, 'deducts the amount from the allowance')
     })
-    it('Allocates the initial supply upon deployment', function() {
-
-        return mikeToken.deployed().then(instance =>{
-            tokenInstance = instance
-            return tokenInstance.totalSupply()
-        }).then (totalSupply =>{
-            assert.equal(totalSupply.toNumber(), 1000000,'sets the total supply of 1,000,000')
-            return tokenInstance.balanceOf(accounts[0])
-        }).then(adminBalance => {
-            assert.equal(adminBalance.toNumber(), 1000000, 'it allocates the initial supupply to admin account ')
-        })
-    });
-
-    it ('transfers token ownership', () => {
-        return mikeToken.deployed().then(instance => {
-            tokenInstance = instance;
-            //should raise a error because the transfer amount far exceeds any possible balance of an given EOA
-            //Note - .call allows us to call our function and inspect but does not create a transaction, so there will be no receipt
-            return tokenInstance.transfer.call(accounts[1],23939239293393)
-        }).then(assert.fail).catch(error =>{
-            assert(error.message.indexOf('revert') >= 0, 'error must contain substring revert, if revert will be at an index greater than 0')
-            //Note - .call allows us to call our function and inspect but does not create a transaction, so there will be no receipt
-            return tokenInstance.transfer.call(accounts[1], 250000, {from:accounts[0]});
-        }).then(success => {
-            assert.equal(success, true, 'it returns true')
-            //Calls the transfer function naturally and return a receipt
-            return tokenInstance.transfer(accounts[1], 250000)
-        }).then(receipt =>{
-            assert.equal(receipt.logs.length, 1, 'since there is only one event, the transaction reciept should be length 1')
-            assert.equal(receipt.logs[0].event, 'Transfer', 'should be the "Transfer" event')
-            assert.equal(receipt.logs[0].args._from, accounts[0], 'logs the account the tokens are transferred from')
-            assert.equal(receipt.logs[0].args._to, accounts[1], 'logs the account the tokens are transferred to')
-            assert.equal(receipt.logs[0].args._value, 250000, 'logs the transfer amount')
-            return tokenInstance.balanceOf(accounts[1])
-        }).then(balance => {
-            assert.equal(balance.toNumber(), 250000, 'adds the amount to the receiving account')
-            return tokenInstance.balanceOf(accounts[0])
-        }).then(balance => {
-            assert.equal(balance.toNumber(), 750000, 'deducts the amount from the sending account')
-        })
-    })
-})
+  });
+});
